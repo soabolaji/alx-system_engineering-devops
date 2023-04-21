cfbdada5510f4a0467ced574394f4f34eaaf50b1
soabolaji <soabolaji@gmail.com>
2023-04-21 13:33:27 -0700

    pippet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # install puppet-lint
-package { 'puppet-lint':
-  ensure   => '2.1.0',
-  provider => 'gem',
+package { 'puppet-lint, python311,flask':
+  ensure   => 'install, 2.1.0',
+  provider => 'pip',
 }
 
